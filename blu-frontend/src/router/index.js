adc3774b636eaf4045abbd3c5a1d2994099393ad
Nicholas Tsimerekis <ntsimerekis@gmail.com>
2025-01-26 07:05:02 +0000

    Finally, router working properly

--- blu-frontend/src/router/index.js
+++ blu-frontend/src/router/index.js
@@ -1,19 +1,19 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 import DashboardView from '@/views/DashboardView.vue'
 import AboutView from '@/views/AboutView.vue'
 import {LoginView, RegisterView} from "@/views";
 
-const routes = [
-    { path: '/', component: DashboardView },
+const my_routes = [
+    { path: '/', name: DashboardView, component: DashboardView },
     { path: '/register', name: "register", component: RegisterView },
     { path: '/about', name: "about", component: AboutView },
     { path: '/login', name: "login", component: LoginView },
 ]
 
 const router = createRouter({
-    history: createMemoryHistory(),
-    routes,
+    history: createWebHistory(),
+    routes : my_routes,
 })
 
 export default router
